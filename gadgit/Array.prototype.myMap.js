1c88cc9981cda16d85ec4643fe6d6b5cb48f3408
Heming <qiaohemingfcb@gmail.com>
2020-10-03 10:17:46 +0800

    modify Array.prototype.myMap.js

--- gadgit/Array.prototype.myMap.js
+++ gadgit/Array.prototype.myMap.js
@@ -1,22 +1,22 @@
 /**
  * map 方法会返回一个新的数组，forEach 则不会返回新的数组。如果需要得到一个新的数组，则使用map，否则一般使用forEach
- * 
+ *
  * @param callback
  * @param thisArg
  * @returns {*}
  */
 Array.prototype.myMap = function (callback, thisArg) {
-    return this.reduce((mappedArray, currentValue, index, array) => {
-        mappedArray[index] = callback.call(thisArg, currentValue, index, array);
-        return mappedArray;
-    }, []);
+  return this.reduce((mappedArray, currentValue, index, array) => {
+    mappedArray[index] = callback.call(thisArg, currentValue, index, array);
+    return mappedArray;
+  }, []);
 }
 
 
 // test
 let array = [32, 1024, 64];
 let resultArr = array.myMap((value, index, array) => {
-    return {value: value, index: index, length: array.length};
+  return {value: value, index: index, length: array.length};
 });
 console.log(resultArr);
 
