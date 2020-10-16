74e03c761929fecb11c623ebef5bf22b1495cabd
Heming <qiaohemingfcb@gmail.com>
2020-10-16 22:28:00 +0800

    add four double pointer solutions

---
+++ algorithms/doublepointer/strstr28/Solution.java
@@ -0,0 +1,39 @@
+package blogandquestion.algorithms.doublepointer.strstr28;
+
+/**
+ * @author Heming
+ * created by IntelliJ IDEA
+ * founded at 2020/10/16 21:43:35
+ * description:
+ *
+ * 实现 strStr() 函数。
+ *
+ * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+ *
+ * 示例 1:
+ *
+ * 输入: haystack = "hello", needle = "ll"
+ * 输出: 2
+ * 示例 2:
+ *
+ * 输入: haystack = "aaaaa", needle = "bba"
+ * 输出: -1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/implement-strstr
+ *
+ * 劝退
+ * see: https://leetcode-cn.com/problems/implement-strstr/solution/shi-xian-strstr-by-leetcode/
+ */
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        int L = needle.length(), n = haystack.length();
+
+        for (int start = 0; start < n - L + 1; ++start) {
+            if (haystack.substring(start, start + L).equals(needle)) {
+                return start;
+            }
+        }
+        return -1;
+    }
+}
