d1b89ac85b351ec993be91ac94783297f43df438
Heming <qiaohemingfcb@gmail.com>
2020-10-17 17:24:06 +0800

    add four solutions to folder binarytree

---
+++ algorithms/binarytree/postordertraversal145/Solution.java
@@ -0,0 +1,61 @@
+package blogandquestion.algorithms.binarytree.postordertraversal145;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Heming
+ * created by IntelliJ IDEA
+ * founded at 2020/10/17 17:16:09
+ * description: 二叉树的后续遍历。
+ *
+ * 给定一个二叉树，返回它的 后序遍历。
+ *
+ * 示例:
+ *
+ * 输入: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * 输出: [3,2,1]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-postorder-traversal
+ *
+ */
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        postorder(root, res);
+        return res;
+    }
+
+    private void postorder(TreeNode root, List<Integer> res) {
+        if (root == null) return; // base case
+        postorder(root.left, res);
+        postorder(root.right, res);
+        res.add(root.val);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ algorithms/binarytree/traversal.md
@@ -0,0 +1,15 @@
+# 二叉树的三种遍历
+
+设根节点为root，左子树为left，右子树为right。
+
+## 先序遍历
+
+root -> left -> right
+
+## 中序遍历
+
+left -> root -> right
+
+## 后序遍历
+
+left -> right -> root
