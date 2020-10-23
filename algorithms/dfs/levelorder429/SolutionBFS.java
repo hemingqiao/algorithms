c32c70c20a35a730766ebe1a9b3652fb95dfca7d
Heming <qiaohemingfcb@gmail.com>
2020-10-23 20:36:57 +0800

    add solutions of levelorder429

---
+++ algorithms/dfs/levelorder429/SolutionBFS.java
@@ -0,0 +1,65 @@
+package blogandquestion.algorithms.dfs.levelorder429;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author Heming
+ * created by IntelliJ IDEA
+ * founded at 2020/10/23 20:11:27
+ * description: 利用BFS层序遍历。执行时间只能击败50。
+ *
+ * 给定一个 N 叉树，返回其节点值的层序遍历。 (即从左到右，逐层遍历)。
+ *
+ * 返回其层序遍历:
+ *
+ * [
+ *      [1],
+ *      [3,2,4],
+ *      [5,6]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/n-ary-tree-level-order-traversal
+ *
+ */
+public class SolutionBFS {
+    public List<List<Integer>> levelOrder(Node root) {
+        if (root == null) return new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> temp = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                Node t = queue.poll();
+                temp.add(t.val);
+                for (int j = 0; j < t.children.size(); j++) {
+                    if (t.children.get(j) != null) queue.add(t.children.get(j));
+                }
+            }
+            res.add(temp);
+        }
+        return res;
+    }
+}
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
