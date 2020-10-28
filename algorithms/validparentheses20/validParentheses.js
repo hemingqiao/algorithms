fb67569cb1ff7925d78f195607520e4285824e9d
Heming <qiaohemingfcb@gmail.com>
2020-10-28 09:19:56 +0800

    update validateparentheses20

---
+++ algorithms/validparentheses20/validParentheses.js
@@ -0,0 +1,48 @@
+const isValid = function (s) {
+  let bracket = [];
+  for (let i = 0; i < s.length; i++) {
+    let ch = s.charAt(i);
+    switch (ch) {
+      case "(":
+      case "[":
+      case "{":
+        bracket.push(ch);
+        break;
+      case ")":
+        if (!!bracket.length) {
+          if (bracket.pop() === "(") {
+            continue;
+          } else {
+            return false;
+          }
+        } else {
+          return false;
+        }
+      case "]":
+        if (!!bracket.length) {
+          if (bracket.pop() === "[") {
+            continue;
+          } else {
+            return false;
+          }
+        } else {
+          return false;
+        }
+      case "}":
+        if (!!bracket.length) {
+          if (bracket.pop() === "{") {
+            continue;
+          } else {
+            return false;
+          }
+        } else {
+          return false;
+        }
+    }
+  }
+  return !bracket.length;
+};
+
+// test
+let result = isValid("]");
+console.log(result);
