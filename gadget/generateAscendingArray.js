dd1f51e34d8605b348e546debe1378a7ccf0511e
Heming <qiaohemingfcb@gmail.com>
2020-11-04 08:57:05 +0800

    add generateAscendingArray.js

---
+++ gadget/generateAscendingArray.js
@@ -0,0 +1,75 @@
+/* 尽量不适用循环语句，从给定的一个数，生成一个升序数组 */
+
+const ge = n => Array.from(Array(n), (v, k) => k + 1);
+
+/**
+ * 不适用循环语句，就使用递归
+ *
+ * @param n
+ * @returns {[]}
+ */
+function gene(n) {
+  let result = [];
+
+  (function fn() {
+    result.unshift(n--);
+    if (n) {
+      fn();
+    }
+  })();
+
+  return result;
+}
+
+console.log(ge(10));
+console.log(gene(10));
+
+// log
+// [
+//   1, 2, 3, 4,  5,
+//   6, 7, 8, 9, 10
+// ]
+// [
+//   1, 2, 3, 4,  5,
+//   6, 7, 8, 9, 10
+// ]
+
+
+/* 下面这种方法使用了循环语句，不过非常巧妙 */
+
+Number.prototype[Symbol.iterator] = function* () {
+  let times = this.valueOf();
+  let i = 0;
+  while (times--) {
+    yield ++i;
+  }
+}
+
+const generateArray = n => [...n];
+
+console.log(generateArray(10));
+
+// log
+// [
+//   1, 2, 3, 4,  5,
+//   6, 7, 8, 9, 10
+// ]
+
+/* -------------------------------函数链式调用------------------------------- */
+
+const add = sum => {
+  const fn = n => add(sum + n);
+  fn.valueOf = () => sum;
+  return fn;
+}
+
+console.log(add(0));
+console.log(add(0)(1));
+console.log(+add(0));
+console.log(+add(0)(1)(2));
+
+// log
+// [Function: fn] { valueOf: [Function] }
+// [Function: fn] { valueOf: [Function] }
+// 0
+// 3
