ea5f5ee57e9bc6995c99f4d0787c4169cfe2c22b
Heming <qiaohemingfcb@gmail.com>
2020-11-06 16:31:49 +0800

    update Array.prototype.myMap.js

--- gadget/Array.prototype.myMap.js
+++ gadget/Array.prototype.myMap.js
@@ -1,3 +1,5 @@
+/* 利用reduce简单实现数组实例的map方法 */
+
 /**
  * map 方法会返回一个新的数组，forEach 则不会返回新的数组。如果需要得到一个新的数组，则使用map，否则一般使用forEach
  *
@@ -27,3 +29,48 @@ console.log(resultArr);
 //   { value: 1024, index: 1, length: 3 },
 //   { value: 64, index: 2, length: 3 }
 // ]
+
+
+
+
+
+/* 简单实现forEach */
+
+/**
+ * forEach方法只是对数组中的每个元素调用一次回调，不会改变原数组，也没有返回值（返回值为undefined）
+ * 
+ * @param callback
+ * @param thisArg
+ */
+Array.prototype.myForEach = function (callback, thisArg) {
+  for (let i = 0; i < this.length; i++) {
+    callback.call(thisArg, this[i], i, this);
+  }
+}
+
+const test = [32, 1024, 64];
+test.myForEach((value, index, array) => {
+  console.log(this.name);
+  console.log(value, index, array.length);
+}, {name: "customize forEach"});
+
+test.forEach((value, index, array) => {
+  console.log(this.name);
+  console.log(value, index, array.length);
+}, {name:"original forEach"});
+
+// log
+// undefined
+// 32 0 3
+// undefined
+// 1024 1 3
+// undefined
+// 64 2 3
+// undefined
+// 32 0 3
+// undefined
+// 1024 1 3
+// undefined
+// 64 2 3
+
+/* 上面输出的this.name均为undefined，是因为在forEach中使用了箭头函数 */
