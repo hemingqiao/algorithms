dc239170df58a4ffe0217e8e785967220752deeb
Heming <qiaohemingfcb@gmail.com>
2020-11-15 16:02:15 +0800

    update subsetswithdup Solution.java

--- algorithms/backtrack/subsetswithdup90/Solution.java
+++ algorithms/backtrack/subsetswithdup90/Solution.java
@@ -9,6 +9,30 @@ import java.util.List;
  * created by IntelliJ IDEA
  * founded at 2020/11/15 15:26:47
  * description:
+ * 给定一个可能包含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ *
+ * 说明：解集不能包含重复的子集。
+ *
+ * 示例:
+ *
+ * 输入: [1,2,2]
+ * 输出:
+ * [
+ *   [2],
+ *   [1],
+ *   [1,2,2],
+ *   [2,2],
+ *   [1,2],
+ *   []
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/subsets-ii
+ *
+ * 参考
+ * https://mp.weixin.qq.com/s/WJ4JNDRJgsW3eUN72Hh3uQ
+ * https://leetcode-cn.com/problems/combination-sum-ii/solution/hui-su-suan-fa-jian-zhi-python-dai-ma-java-dai-m-3/225211
+ * https://leetcode-cn.com/problems/subsets-ii/solution/li-jie-li-jie-qu-zhong-cao-zuo-by-jin-ai-yi/
  */
 public class Solution {
     List<List<Integer>> res = new ArrayList<>();
