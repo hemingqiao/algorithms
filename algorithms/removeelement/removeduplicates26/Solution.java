73eb0045ac8632ea2473d0421b05b09e9caf5e89
Heming <qiaohemingfcb@gmail.com>
2020-11-17 21:07:55 +0800

    update removeduplicates26 Solution.java

--- algorithms/removeelement/removeduplicates26/Solution.java
+++ algorithms/removeelement/removeduplicates26/Solution.java
@@ -59,3 +59,19 @@ public class Solution {
         return index + 1;
     }
 }
+
+/**
+ * 参考：算法珠玑，algorithm-essentials-java.pdf
+ */
+class AnotherSolution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int index = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[index - 1]) {
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+}
