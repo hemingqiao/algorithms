3e9a35d3f0116b731b09374fe633838ecb085287
Heming <qiaohemingfcb@gmail.com>
2020-11-23 22:24:03 +0800

    add solution to dailytemperatures739

---
+++ algorithms/stack/monotonestack/dailytemperatures739/Solution.java
@@ -0,0 +1,37 @@
+package blogandquestion.algorithms.stack.monotonestack.dailytemperatures739;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author Heming
+ * created by IntelliJ IDEA
+ * founded at 2020/11/23 22:12:07
+ * description:
+ * 请根据每日 气温 列表，重新生成一个列表。对应位置的输出为：要想观测到更高的气温，至少需要等待的天数。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+ *
+ * 例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]。
+ *
+ * 提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/daily-temperatures
+ *
+ * 参考：https://leetcode-cn.com/problems/daily-temperatures/solution/leetcode-tu-jie-739mei-ri-wen-du-by-misterbooo/
+ */
+public class Solution {
+    public int[] dailyTemperatures(int[] T) {
+        int len = T.length;
+        int[] res = new int[len];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < len; i++) {
+            // 维护一个单调递减栈
+            while (!stack.isEmpty() && T[i] > T[stack.peek()]) {
+                int idx = stack.pop();
+                res[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
