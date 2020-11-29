47bf67165370a5a268eda13796690a27168af227
Heming <qiaohemingfcb@gmail.com>
2020-11-29 20:39:29 +0800

    update solution to fib509

--- algorithms/numbers/fib509/Solution.java
+++ algorithms/numbers/fib509/Solution.java
@@ -24,3 +24,30 @@ public class Solution {
         }
     }
 }
+
+class AnotherSolution {
+    // 记忆化
+    public int fib(int n) {
+        return fibAux(n, new int[n + 1]);
+    }
+
+    // 求解某个斐波那契数时，会一直递归到最底层的base case处，然后计算一些值，在此时这些最底层的值被缓存了下来
+    // 求解上层的值时就方便很多
+    public int fibAux(int n, int[] memo) {
+        if (n == 0) return 0;
+        else if (n == 1) return 1;
+        if (memo[n] != 0) {
+            return memo[n];
+        }
+        int res = fibAux(n - 1, memo) + fibAux(n - 2, memo);
+        memo[n] = res;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        AnotherSolution as = new AnotherSolution();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(as.fib(i));
+        }
+    }
+}
