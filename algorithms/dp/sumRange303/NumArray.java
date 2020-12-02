3d3dab6a13d2baef2fb67c8ff48d006b44d7f96d
Heming <qiaohemingfcb@gmail.com>
2020-12-02 13:25:00 +0800

    add solutions to sumRange303 and sumrange304

--- algorithms/dp/sumRange303/NumArray.java
+++ algorithms/dp/sumRange303/NumArray.java
@@ -6,19 +6,34 @@ package blogandquestion.algorithms.dp.sumRange303;
  * founded at 2020/10/06 19:33:57
  * description: 利用构造器将sumRange(0, k)的和预先求出来并缓存到一个数组中，这样后面的计算的时间复杂度就是O(1)了
  *
- * 给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点。
+ *
+ * 实现 NumArray 类：
+ *
+ * NumArray(int[] nums) 使用数组 nums 初始化对象
+ * int sumRange(int i, int j) 返回数组 nums 从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点（也就是 sum(nums[i], nums[i + 1], ... , nums[j])）
  *
  * 示例：
  *
- * 给定 nums = [-2, 0, 3, -5, 2, -1]，求和函数为 sumRange()
+ * 输入：
+ * ["NumArray", "sumRange", "sumRange", "sumRange"]
+ * [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+ * 输出：
+ * [null, 1, -1, -3]
  *
- * sumRange(0, 2) -> 1
- * sumRange(2, 5) -> -1
- * sumRange(0, 5) -> -3
- * 说明:
+ * 解释：
+ * NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+ * numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+ * numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1))
+ * numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+ *  
  *
- * 你可以假设数组不可变。
- * 会多次调用 sumRange 方法。
+ * 提示：
+ *
+ * 0 <= nums.length <= 104
+ * -105 <= nums[i] <= 105
+ * 0 <= i <= j < nums.length
+ * 最多调用 104 次 sumRange 方法
  *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/range-sum-query-immutable
@@ -44,6 +59,25 @@ public class NumArray {
         // 这里用到了动态规划的思想
         // sumRange(i, j) = sumRange(0, j) - sumRange(0, i - 1)
         return sums[j] - sums[i - 1];
+    }
+}
+
+/**
+ * 改为了dp数组形式
+ */
+class AnotherSolution {
+    private int[] dp;
+    public AnotherSolution(int[] nums) {
+        if (nums.length == 0) return;
+        int len = nums.length;
+        dp = new int[len + 1];
+        for (int i = 0; i < len; i++) {
+            dp[i + 1] = dp[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return dp[j + 1] - dp[i + 1];
     }
 }
 
---
+++ algorithms/dp/sumrange304/NumMatrix.java
@@ -0,0 +1,55 @@
+package blogandquestion.algorithms.dp.sumrange304;
+
+/**
+ * @author Heming
+ * created by IntelliJ IDEA
+ * founded at 2020/12/02 12:53:07
+ * description:
+ * 给定一个二维矩阵，计算其子矩形范围内元素的总和，该子矩阵的左上角为 (row1, col1) ，右下角为 (row2, col2)。
+ *
+ * 上图子矩阵左上角 (row1, col1) = (2, 1) ，右下角(row2, col2) = (4, 3)，该子矩形内元素的总和为 8。
+ *
+ * 示例:
+ *
+ * 给定 matrix = [
+ *   [3, 0, 1, 4, 2],
+ *   [5, 6, 3, 2, 1],
+ *   [1, 2, 0, 1, 5],
+ *   [4, 1, 0, 1, 7],
+ *   [1, 0, 3, 0, 5]
+ * ]
+ *
+ * sumRegion(2, 1, 4, 3) -> 8
+ * sumRegion(1, 1, 2, 2) -> 11
+ * sumRegion(1, 2, 2, 4) -> 12
+ * 说明:
+ *
+ * 你可以假设矩阵不可变。
+ * 会多次调用 sumRegion 方法。
+ * 你可以假设 row1 ≤ row2 且 col1 ≤ col2。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/range-sum-query-2d-immutable
+ *
+ * 参考：https://leetcode-cn.com/problems/range-sum-query-2d-immutable/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-57/
+ */
+public class NumMatrix {
+    private int[][] dp;
+
+    public NumMatrix(int[][] matrix) {
+        if (matrix.length == 0) return;
+        int row = matrix.length, col = matrix[0].length;
+        dp = new int[row + 1][col + 1];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                // 对于一个四方格子，格子右下方的值[i + 1][j + 1]等于[i + 1][j]（左侧）的值加上[i][j + 1]（上方）值与当前元素的值之和
+                // 但是[i][j]处加了两次，需要减去一次，就有了下面的状态转移方程
+                dp[i + 1][j + 1] = matrix[i][j] + dp[i + 1][j] + dp[i][j + 1] - dp[i][j];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return dp[row2 + 1][col2 + 1] + dp[row1][col1] - dp[row2 + 1][col1] - dp[row1][col2 + 1];
+    }
+}
