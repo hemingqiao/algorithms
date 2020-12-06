a5e60664156e7725fb05e84bd7bc77fb1ba7eb62
Heming <qiaohemingfcb@gmail.com>
2020-12-06 16:38:38 +0800

    update solutions to lowestcommonancestor236

--- algorithms/binarytree/lowestcommonancestor236/AnotherSolution.java
+++ algorithms/binarytree/lowestcommonancestor236/AnotherSolution.java
@@ -7,8 +7,18 @@ package blogandquestion.algorithms.binarytree.lowestcommonancestor236;
  * description:
  * 参考：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/javadai-ma-di-gui-he-fei-di-gui-tu-wen-xiang-jie-b/
  * 参考：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/c-jing-dian-di-gui-si-lu-fei-chang-hao-li-jie-shi-/
+ *
+ * 参考：https://leetcode-cn.com/problems/er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof/solution/mian-shi-ti-68-ii-er-cha-shu-de-zui-jin-gong-gon-7/
  */
 public class AnotherSolution {
+    // 题目限定了p和q一定都在二叉树中存在，那么如果当前结点不等于p或q，p和q要么分别位于左右子树中，要么同时位于左子树，或者同时位于右子树，
+    // 那么分别来讨论：
+    //- 若p和q分别位于左右子树中，那么对左右子结点调用递归函数，会分别返回p和q结点的位置，而当前结点正好就是p和q的最小共同父结点，
+    // 直接返回当前结点即可，这就是题目中给出的例子1的情况。
+    //- 若p和q同时位于左子树，这里有两种情况，一种情况是 left 会返回p和q中较高的那个位置，而 right 会返回空，所以最终返回非空的 left 即可，
+    // 这就是题目中的例子2的情况。还有一种情况是会返回p和q的最小父结点，就是说当前结点的左子树中的某个结点才是p和q的最小父结点，会被返回。
+    //- 若p和q同时位于右子树，同样有两种情况，一种情况是 right 会返回p和q中较高的那个位置，而 left 会返回空，所以最终返回非空的 right 即可，
+    // 还有一种情况是会返回p和q的最小父结点，就是说当前结点的右子树中的某个结点才是p和q的最小父结点，会被返回
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root == p || root == q) {
             return root;
---
+++ algorithms/binarytree/lowestcommonancestor236/lowestcommonancestor.md
@@ -0,0 +1,3 @@
+#二叉树的最近公共祖先
+
+参考：https://leetcode-cn.com/problems/er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof/solution/mian-shi-ti-68-ii-er-cha-shu-de-zui-jin-gong-gon-7/
