7f4b78c1f8f66d550ad78590b9253cd77df13c67
Heming <qiaohemingfcb@gmail.com>
2020-12-13 16:07:39 +0800

    update flatten114

--- algorithms/binarytree/flatten114/Solution.java
+++ algorithms/binarytree/flatten114/Solution.java
@@ -51,6 +51,25 @@ public class Solution {
     }
 }
 
+/**
+ * 参考：https://github.com/grandyang/leetcode/issues/114
+ */
+class AnotherSolution {
+    // 先找到最左的节点，将这个节点挂在其父节点的右子树上，而原先的右子节点挂在新的右子节点的右子树上
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+        if (root.left != null) flatten(root.left);
+        if (root.right != null) flatten(root.right);
+        TreeNode temp = root.right;
+        root.right = root.left; // 将最左的节点的左子树挂在右子树上
+        root.left = null; // 将最左的节点的左子树置为空
+        while (root.right != null) {
+            root = root.right;
+        }
+        root.right = temp; // 将原先的右子节点挂在新的右子节点的右子树上
+    }
+}
+
 class TreeNode {
     int val;
     TreeNode left;
