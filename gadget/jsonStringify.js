5dc1d20cca5d5ae69d1da835bf791f860699a3b2
Heming <qiaohemingfcb@gmail.com>
2020-12-20 10:15:32 +0800

    update

--- gadget/jsonStringify.js
+++ gadget/jsonStringify.js
@@ -2,7 +2,11 @@
 @author Heming
 founded at 2020-12-03 21:30:22
 created by WebStorm
-description: 简单模拟JSON.stringify方法
+description: 简单模拟JSON.stringify方法和JSON.parse
+
+关于JSON.parse，参考：
+https://github.com/douglascrockford/JSON-js
+https://juejin.cn/post/6844903568919527432
 ***************************************************************************** */
 
 /**
@@ -17,13 +21,13 @@ function jsonStringify(obj) {
     return "undefined";
   }
 
-  // 首先排除null
   if (obj === null) {
     return "null";
   } else if (typeof obj === "object") {
     let isArray = Array.isArray(obj);
     let res = [];
-    for (let key in obj) {
+    let keys = Object.keys(obj)
+    for (let key of keys) {
       let v = obj[key];
 
       // 特判undefined和function
@@ -36,14 +40,15 @@ function jsonStringify(obj) {
         continue;
       }
 
-      // 先排除掉null（外层对null已经做过判断，此处可以删去）
       if (v === null) {
         v = "null";
       } else if (typeof v === "object") {
         v = jsonStringify(v);
       } else {
-        // 值不是数字时拼上双引号
-        if (typeof v !== "number") {
+        // 值为数字或者布尔类型时不拼双引号
+        if (typeof v === "number" || typeof v === "boolean") {
+          v = String(v);
+        } else {
           v = '"' + String(v) + '"';
         }
       }
@@ -53,9 +58,14 @@ function jsonStringify(obj) {
     // 根据原对象是否为数组拼接[]或者{}
     return (isArray ? "[" : "{") + String(res) + (isArray ? "]" : "}");
   } else {
-    // 处理obj为其他非object类型的值
+    // 处理obj为其他非object类型的值的情况
     return String(obj);
   }
+}
+
+
+function jsonParse(json) {
+  return eval("(" + json + ")");
 }
 
 
@@ -79,7 +89,9 @@ let a = {
       console.log("foo");
     }
   },
-  i: {j: null, k: undefined, l: function (){}}
+  i: {j: null, k: undefined, l: function (){}},
+  j: true,
+  k: false
 };
 
 let a1 = {
@@ -91,47 +103,13 @@ console.log(JSON.stringify(a));
 let res1 = jsonStringify(a);
 console.log(res1);
 
-const ancestor = [
-  {"name": "Carolus Haverbeke", "sex": "m", "born": 1832, "died": 1905, "father": "Carel Haverbeke", "mother": "Maria van Brussel"},
-  {"name": "Emma de Milliano", "sex": "f", "born": 1876, "died": 1956, "father": "Petrus de Milliano", "mother": "Sophia van Damme"},
-  {"name": "Maria de Rycke", "sex": "f", "born": 1683, "died": 1724, "father": "Frederik de Rycke", "mother": "Laurentia van Vlaenderen"},
-  {"name": "Jan van Brussel", "sex": "m", "born": 1714, "died": 1748, "father": "Jacobus van Brussel", "mother": "Joanna van Rooten"},
-  {"name": "Philibert Haverbeke", "sex": "m", "born": 1907, "died": 1997, "father": "Emile Haverbeke", "mother": "Emma de Milliano"},
-  {"name": "Jan Frans van Brussel", "sex": "m", "born": 1761, "died": 1833, "father": "Jacobus Bernardus van Brussel", "mother":null},
-  {"name": "Pauwels van Haverbeke", "sex": "m", "born": 1535, "died": 1582, "father": "N. van Haverbeke", "mother":null},
-  {"name": "Clara Aernoudts", "sex": "f", "born": 1918, "died": 2012, "father": "Henry Aernoudts", "mother": "Sidonie Coene"},
-  {"name": "Emile Haverbeke", "sex": "m", "born": 1877, "died": 1968, "father": "Carolus Haverbeke", "mother": "Maria Sturm"},
-  {"name": "Lieven de Causmaecker", "sex": "m", "born": 1696, "died": 1724, "father": "Carel de Causmaecker", "mother": "Joanna Claes"},
-  {"name": "Pieter Haverbeke", "sex": "m", "born": 1602, "died": 1642, "father": "Lieven van Haverbeke", "mother":null},
-  {"name": "Livina Haverbeke", "sex": "f", "born": 1692, "died": 1743, "father": "Daniel Haverbeke", "mother": "Joanna de Pape"},
-  {"name": "Pieter Bernard Haverbeke", "sex": "m", "born": 1695, "died": 1762, "father": "Willem Haverbeke", "mother": "Petronella Wauters"},
-  {"name": "Lieven van Haverbeke", "sex": "m", "born": 1570, "died": 1636, "father": "Pauwels van Haverbeke", "mother": "Lievijne Jans"},
-  {"name": "Joanna de Causmaecker", "sex": "f", "born": 1762, "died": 1807, "father": "Bernardus de Causmaecker", "mother":null},
-  {"name": "Willem Haverbeke", "sex": "m", "born": 1668, "died": 1731, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"},
-  {"name": "Pieter Antone Haverbeke", "sex": "m", "born": 1753, "died": 1798, "father": "Jan Francies Haverbeke", "mother": "Petronella de Decker"},
-  {"name": "Maria van Brussel", "sex": "f", "born": 1801, "died": 1834, "father": "Jan Frans van Brussel", "mother": "Joanna de Causmaecker"},
-  {"name": "Angela Haverbeke", "sex": "f", "born": 1728, "died": 1734, "father": "Pieter Bernard Haverbeke", "mother": "Livina de Vrieze"},
-  {"name": "Elisabeth Haverbeke", "sex": "f", "born": 1711, "died": 1754, "father": "Jan Haverbeke", "mother": "Maria de Rycke"},
-  {"name": "Lievijne Jans", "sex": "f", "born": 1542, "died": 1582, "father":null, "mother":null},
-  {"name": "Bernardus de Causmaecker", "sex": "m", "born": 1721, "died": 1789, "father": "Lieven de Causmaecker", "mother": "Livina Haverbeke"},
-  {"name": "Jacoba Lammens", "sex": "f", "born": 1699, "died": 1740, "father": "Lieven Lammens", "mother": "Livina de Vrieze"},
-  {"name": "Pieter de Decker", "sex": "m", "born": 1705, "died": 1780, "father": "Joos de Decker", "mother": "Petronella van de Steene"},
-  {"name": "Joanna de Pape", "sex": "f", "born": 1654, "died": 1723, "father": "Vincent de Pape", "mother": "Petronella Wauters"},
-  {"name": "Daniel Haverbeke", "sex": "m", "born": 1652, "died": 1723, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"},
-  {"name": "Lieven Haverbeke", "sex": "m", "born": 1631, "died": 1676, "father": "Pieter Haverbeke", "mother": "Anna van Hecke"},
-  {"name": "Martina de Pape", "sex": "f", "born": 1666, "died": 1727, "father": "Vincent de Pape", "mother": "Petronella Wauters"},
-  {"name": "Jan Francies Haverbeke", "sex": "m", "born": 1725, "died": 1779, "father": "Pieter Bernard Haverbeke", "mother": "Livina de Vrieze"},
-  {"name": "Maria Haverbeke", "sex": "m", "born": 1905, "died": 1997, "father": "Emile Haverbeke", "mother": "Emma de Milliano"},
-  {"name": "Petronella de Decker", "sex": "f", "born": 1731, "died": 1781, "father": "Pieter de Decker", "mother": "Livina Haverbeke"},
-  {"name": "Livina Sierens", "sex": "f", "born": 1761, "died": 1826, "father": "Jan Sierens", "mother": "Maria van Waes"},
-  {"name": "Laurentia Haverbeke", "sex": "f", "born": 1710, "died": 1786, "father": "Jan Haverbeke", "mother": "Maria de Rycke"},
-  {"name": "Carel Haverbeke", "sex": "m", "born": 1796, "died": 1837, "father": "Pieter Antone Haverbeke", "mother": "Livina Sierens"},
-  {"name": "Elisabeth Hercke", "sex": "f", "born": 1632, "died": 1674, "father": "Willem Hercke", "mother": "Margriet de Brabander"},
-  {"name": "Jan Haverbeke", "sex": "m", "born": 1671, "died": 1731, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"},
-  {"name": "Anna van Hecke", "sex": "f", "born": 1607, "died": 1670, "father": "Paschasius van Hecke", "mother": "Martijntken Beelaert"},
-  {"name": "Maria Sturm", "sex": "f", "born": 1835, "died": 1917, "father": "Charles Sturm", "mother": "Seraphina Spelier"},
-  {"name": "Jacobus Bernardus van Brussel", "sex": "m", "born": 1736, "died": 1809, "father": "Jan van Brussel", "mother": "Elisabeth Haverbeke"}
-];
-// console.log(jsonStringify(ancestor));
-// console.log(JSON.stringify(ancestor));
 
+console.log(jsonStringify({
+  "a": 1,
+  "b": true,
+  "c": null,
+  "d": [1, 2, {"x": 5, "y": 6, "z": {"i": "foobar", "j": 3.45}}]
+}));
+
+console.log(jsonStringify([1, 2, 3, 4, 5]));
+console.log(jsonStringify({"a": 1}));
