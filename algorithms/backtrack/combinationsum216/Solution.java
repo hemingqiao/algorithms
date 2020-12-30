e6ba5b95e58bb7ecf0c529a182d6af3152d84535
Heming <qiaohemingfcb@gmail.com>
2020-12-30 19:26:07 +0800

    udpate solutions to combinationsum

--- algorithms/backtrack/combinationsum216/Solution.java
+++ algorithms/backtrack/combinationsum216/Solution.java
@@ -93,3 +93,24 @@ class AnotherSolution {
         }
     }
 }
+
+class ThirdSolution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        dfs(0, n, k, path, res, 1);
+        return res;
+    }
+
+    private void dfs(int sum, int target, int k, List<Integer> path, List<List<Integer>> res, int startIndex) {
+        if (path.size() == k) {
+            if (sum == target) res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = startIndex; i <= 9; i++) {
+            path.add(i);
+            dfs(sum + i, target, k, path, res, i + 1);
+            path.remove(path.size() - 1);
+        }
+    }
+}
