c916a4fb7dc51a16037dd57bdf2ee390083f5282
Heming <qiaohemingfcb@gmail.com>
2021-01-20 19:31:31 +0800

    update solutions to swappairs24

---
+++ algorithms/linkedlist/swappairs24/ExerciseSolution.java
@@ -0,0 +1,38 @@
+package blogandquestion.algorithms.linkedlist.swappairs24;
+
+/**
+ * @author Heming
+ * created by IntelliJ IDEA
+ * founded at 2021/01/20 19:06:15
+ * description:
+ * 二刷
+ */
+public class ExerciseSolution {
+    // 递归，重点在于理解递归是将大的问题分解为一个相同（类似）的规模更小的子问题，可以假设函数已经写好
+    // 递归的出口需要注意
+    public ListNode swapPairs(ListNode head) {
+        if (head == null) return null;
+        if (head.next == null) return head;
+        ListNode second = head.next;
+        head.next = second.next;
+        second.next = head;
+        head.next = swapPairs(head.next);
+        return second;
+    }
+
+    // 非递归解法
+    // 参考：https://github.com/grandyang/leetcode/issues/24
+    public ListNode swapPairs1(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode pre = dummy;
+        while (pre.next != null && pre.next.next != null) {
+            ListNode temp = pre.next.next;
+            pre.next.next = temp.next;
+            temp.next = pre.next;
+            pre.next = temp;
+            pre = temp.next;
+        }
+        return dummy.next;
+    }
+}
