93161d879fb036dc3627a53530bdeb75808d4d66
Heming <qiaohemingfcb@gmail.com>
2021-01-23 15:09:04 +0800

    update myInstanceof.js

--- gadget/myInstanceof.js
+++ gadget/myInstanceof.js
@@ -2,29 +2,60 @@
 
 
 /**
- * A instanceof B 的原理就是判断B的原型对象是否存在于A的原型链上
- *
+ * 模拟实现 instanceof （instanceof的原理就是检查运算符右侧的函数的原型对象是否存在于运算法左侧的对象的原型链之上）
+ * 需要注意的是运算符右侧必须是函数，且这个函数的prototype属性不能为非对象。
  * @param A
  * @param B
- * @returns {boolean}
- * @constructor
+ * @return {boolean}
  */
-function MyInstanceof(A, B) {
-  var rightPrototype = B.prototype;
-  var leftProto = Object.getPrototypeOf(A);
+function myInstanceof(A, B) {
+  if (!B || typeof B !== "object") {
+    throw new TypeError("Right-hand side of 'instanceof' is not an object");
+  }
+  if (typeof B !== "function") {
+    throw new TypeError("Right-hand side of 'instanceof' is not callable");
+  }
 
-  while (true) {
-    if (rightPrototype == null) {
-      throw new Error();
+  let rightPrototype = B.prototype,
+      leftProto = Object.getPrototypeOf(A);
+  
+  while (leftProto != null) {
+    if (!rightPrototype) {
+      throw new TypeError("Function has non-object prototype in instanceof check");
     }
-
-    if (leftProto == null) {
-      return false;
-    }
-
-    if (leftProto === rightPrototype) {
+    if (leftProto == rightPrototype) {
       return true;
     }
     leftProto = Object.getPrototypeOf(leftProto);
   }
+  return false;
 }
+
+
+// /**
+//  * A instanceof B 的原理就是判断B的原型对象是否存在于A的原型链上
+//  *
+//  * @param A
+//  * @param B
+//  * @returns {boolean}
+//  * @constructor
+//  */
+// function MyInstanceof(A, B) {
+//   var rightPrototype = B.prototype;
+//   var leftProto = Object.getPrototypeOf(A);
+
+//   while (true) {
+//     if (rightPrototype == null) {
+//       throw new Error();
+//     }
+
+//     if (leftProto == null) {
+//       return false;
+//     }
+
+//     if (leftProto === rightPrototype) {
+//       return true;
+//     }
+//     leftProto = Object.getPrototypeOf(leftProto);
+//   }
+// }
