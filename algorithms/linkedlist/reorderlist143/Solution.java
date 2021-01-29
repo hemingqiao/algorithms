74b721eadd352806f7617c6cff6c6ef997e0262c
Heming <qiaohemingfcb@gmail.com>
2021-01-29 21:38:36 +0800

    add solutions to reorderlist143

---
+++ algorithms/linkedlist/reorderlist143/Solution.java
@@ -0,0 +1,68 @@
+package blogandquestion.algorithms.linkedlist.reorderlist143;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Heming
+ * created by IntelliJ IDEA
+ * founded at 2020/10/20 21:52:03
+ * description:
+ *
+ * 给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+ * 将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+ *
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ *
+ * 示例 1:
+ *
+ * 给定链表 1->2->3->4, 重新排列为 1->4->2->3.
+ * 示例 2:
+ *
+ * 给定链表 1->2->3->4->5, 重新排列为 1->5->2->4->3.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reorder-list
+ *
+ * 解题思路
+ * see: https://leetcode-cn.com/problems/reorder-list/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-34/
+ */
+public class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null) return;
+        List<ListNode> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head);
+            head = head.next;
+        }
+
+        int i = 0, j = list.size() - 1;
+        while (i < j) {
+            list.get(i).next = list.get(j);
+            i++;
+            if (i == j) { // 偶数个节点时会提前相遇
+                break;
+            }
+            list.get(j).next = list.get(i);
+            j--;
+        }
+        list.get(j).next = null;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
