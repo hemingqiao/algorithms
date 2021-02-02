6c2fe803ad3e9adca2b2d208ece17f4df807a71f
Heming <qiaohemingfcb@gmail.com>
2021-02-02 20:04:37 +0800

    update solutions to spiralorder54

--- algorithms/numbers/spiralorder54/AnotherSolution.java
+++ algorithms/numbers/spiralorder54/AnotherSolution.java
@@ -33,4 +33,21 @@ public class AnotherSolution {
         }
         return res;
     }
+
+    // 逆时针螺旋遍历
+    public List<Integer> spiralOrderReverse(int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+        int left = 0, right = matrix[0].length - 1, top = 0, bottom = matrix.length - 1;
+        while (true) {
+            for (int i = top; i <= bottom; i++) list.add(matrix[i][left]);
+            if (++left > right) break;
+            for (int i = left; i <= right; i++) list.add(matrix[bottom][i]);
+            if (--bottom < top) break;
+            for (int i = bottom; i >= top; i--) list.add(matrix[i][right]);
+            if (--right < left) break;
+            for (int i = right; i >= left; i--) list.add(matrix[top][i]);
+            if (++top > bottom) break;
+        }
+        return list;
+    }
 }
