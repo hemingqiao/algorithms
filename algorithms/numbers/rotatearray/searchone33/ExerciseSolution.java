8cc6bce0c9d4ea45907a6d1da722c2551c70e779
Heming <qiaohemingfcb@gmail.com>
2021-02-05 20:49:50 +0800

    update solutions to searchone33

--- algorithms/numbers/rotatearray/searchone33/ExerciseSolution.java
+++ algorithms/numbers/rotatearray/searchone33/ExerciseSolution.java
@@ -10,7 +10,7 @@ package blogandquestion.algorithms.numbers.rotatearray.searchone33;
  */
 public class ExerciseSolution {
     // 二分搜索法的关键在于获得了中间数后，判断下面要搜索左半段还是右半段，通过观察可以得出规律，如果中间的数小于最右边的数，则右半段是有序的，
-    // 若中间数大于最右边数，则左半段是有序的，我们只要在有序的半段里用首尾两个数组来判断目标值是否在这一区域内，这样就可以确定保留哪半边了
+    // 若中间数大于最右边数，则左半段是有序的，只要在有序的半段里用首尾两个数组来判断目标值是否在这一区域内，这样就可以确定保留哪半边了
     public int search(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
         while (left <= right) {
@@ -35,5 +35,30 @@ public class ExerciseSolution {
         int[] test = new int[]{3,5,1};
         int res = es.search(test, 3);
         System.out.println(res);
+    }
+
+    // 通过与数组最左边的值比较来确定有序的区间，上面的search是通过与数组最右边的值进行比较来确定有序区间的
+    public int search1(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < nums[left]) {
+                // 此时区间[mid, right]是有序的，判断target是否位于有序的区间内
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                // 此时区间[left, mid]是有序的，首先判断target是否位于有序区间内
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
     }
 }
