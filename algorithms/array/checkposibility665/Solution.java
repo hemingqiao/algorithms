421dc1cdc1a4878f4903936dade90115d98bf7e9
Heming <qiaohemingfcb@gmail.com>
2021-02-07 09:51:53 +0800

    add solution to checkposibility665

---
+++ algorithms/array/checkposibility665/Solution.java
@@ -0,0 +1,49 @@
+package blogandquestion.algorithms.array.checkposibility665;
+
+/**
+ * @author Heming
+ * created by IntelliJ IDEA
+ * founded at 2021/02/07 09:46:34
+ * description:
+ * 给你一个长度为 n 的整数数组，请你判断在 最多 改变 1 个元素的情况下，该数组能否变成一个非递减数列。
+ *
+ * 我们是这样定义一个非递减数列的： 对于数组中所有的 i (0 <= i <= n-2)，总满足 nums[i] <= nums[i + 1]。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [4,2,3]
+ * 输出: true
+ * 解释: 你可以通过把第一个4变成1来使得它成为一个非递减数列。
+ * 示例 2:
+ *
+ * 输入: nums = [4,2,1]
+ * 输出: false
+ * 解释: 你不能在只改变一个元素的情况下将其变为非递减数列。
+ *  
+ *
+ * 说明：
+ *
+ * 1 <= n <= 10 ^ 4
+ * - 10 ^ 5 <= nums[i] <= 10 ^ 5
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/non-decreasing-array
+ *
+ * 参考：https://github.com/grandyang/leetcode/issues/665
+ */
+public class Solution {
+    public boolean checkPosibility(int[] nums) {
+        int count = 1, n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[i - 1]) {
+                if (count == 0) return false;
+                if (i == 1 || nums[i] >= nums[i - 2]) nums[i - 1] = nums[i];
+                else nums[i] = nums[i - 1];
+                count--;
+            }
+        }
+        return true;
+    }
+}
