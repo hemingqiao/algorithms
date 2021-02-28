662684300d8e5a72a44d6508e69981af3e7d68b8
Heming <qiaohemingfcb@gmail.com>
2021-02-28 21:08:36 +0800

    update solutions to fib509

--- algorithms/numbers/fib509/solution.js
+++ algorithms/numbers/fib509/solution.js
@@ -16,3 +16,16 @@ const fib = function (N) {
 for (let i = 0; i < 10; i++) {
     console.log(fib(i));
 }
+
+// memorize version
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const fib1 = function(n, cache = []) {
+    if (cache[n]) return cache[n];
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return cache[n] = fib1(n - 1, cache) + fib1(n - 2, cache);
+};
