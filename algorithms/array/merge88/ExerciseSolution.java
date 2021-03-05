c114ab3923bcf5091754caf3eef90e03f857dbfd
Heming <qiaohemingfcb@gmail.com>
2021-03-05 14:10:58 +0800

    update solutions to merge88

--- algorithms/array/merge88/ExerciseSolution.java
+++ algorithms/array/merge88/ExerciseSolution.java
@@ -29,4 +29,35 @@ public class ExerciseSolution {
             nums1[end--] = t;
         }
     }
+
+    // 有点丑这代码
+    // lcci面试题，https://leetcode-cn.com/problems/sorted-merge-lcci/
+    public void merge2(int[] A, int m, int[] B, int n) {
+        int p = m + n - 1;
+        int i, j;
+        for (i = m - 1, j = n - 1; i >= 0 && j >= 0;) {
+            int a = A[i], b = B[j];
+            if (a >= b) {
+                A[p--] = a;
+                i--;
+            } else {
+                A[p--] = b;
+                j--;
+            }
+        }
+        while (j >= 0) {
+            A[p--] = B[j--];
+        }
+    }
+
+    public void merge3(int[] A, int m, int[] B, int n) {
+        int p = m + n - 1;
+        int i, j;
+        for (i = m - 1, j = n - 1; i >= 0 && j >= 0;) {
+            A[p--] = A[i] >= B[j] ? A[i--] : B[j--];
+        }
+        while (j >= 0) {
+            A[p--] = B[j--];
+        }
+    }
 }
