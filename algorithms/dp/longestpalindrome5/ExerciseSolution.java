d426d2b93c0063fe2ae6d30d214f7c08726d2c44
Heming <qiaohemingfcb@gmail.com>
2021-05-26 22:12:08 +0800

    update solutions to longestpalindrome5

---
+++ algorithms/dp/longestpalindrome5/ExerciseSolution.java
@@ -0,0 +1,39 @@
+package blogandquestion.algorithms.dp.longestpalindrome5;
+
+/**
+ * @author Heming
+ * created by IntelliJ IDEA
+ * founded at 2021/01/10 19:09:41
+ * description: 中心扩散法
+ */
+public class ExerciseSolution {
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len <= 2) return s;
+        String res = s.substring(0, 1);
+        int maxLen = 0;
+        for (int i = 0; i < len - 1; i++) {
+            String evenStr = getPalindromeStr(s, i, i + 1, len);
+            String oddStr = getPalindromeStr(s, i, i, len);
+            String maxLenStr = evenStr.length() > oddStr.length() ? evenStr : oddStr;
+            if (maxLenStr.length() > maxLen) {
+                maxLen = maxLenStr.length();
+                res = maxLenStr;
+            }
+        }
+        return res;
+    }
+
+    private String getPalindromeStr(String s, int left, int right, int len) {
+        int l = left, r = right;
+        while (l >= 0 && r < len) {
+            if (s.charAt(l) == s.charAt(r)) {
+                l--;
+                r++;
+            } else {
+                break;
+            }
+        }
+        return s.substring(++l, r);
+    }
+}
---
+++ algorithms/dp/longestpalindrome5/ExerciseSolution2.java
@@ -0,0 +1,33 @@
+package blogandquestion.algorithms.dp.longestpalindrome5;
+
+/**
+ * @author Heming
+ * created by IntelliJ IDEA
+ * founded at 2021/05/26 22:10:10
+ * description:
+ */
+public class ExerciseSolution2 {
+    public String longestPalindrome(String s) {
+        char[] chars = s.toCharArray();
+        int n = chars.length;
+        if (n == 0) return "";
+        // dp[i][j]表示序列[i, j]代表的字符串是否是回文串
+        boolean[][] dp = new boolean[n][n];
+        int left = 0, len = 1;
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+            for (int j = 0; j < i; j++) {
+                if (i - j == 1) {
+                    dp[i][j] = chars[i] == chars[j];
+                } else if(i - j > 1) {
+                    dp[i][j] = chars[i] == chars[j] && dp[i - 1][j + 1];
+                }
+                if (dp[i][j] && i - j + 1 > len) {
+                    len = i - j + 1;
+                    left = j;
+                }
+            }
+        }
+        return s.substring(left, left + len);
+    }
+}
