ebc4c2498eeb84f40755c1ef7e7f5f2595050a7a
Heming <qiaohemingfcb@gmail.com>
2021-07-11 08:46:40 +0800

    update solutions to majorityelement229, add solution to majorityelementlcci

--- algorithms/array/majorityelement229/Solution.java
+++ algorithms/array/majorityelement229/Solution.java
@@ -9,7 +9,7 @@ import java.util.List;
  * created by IntelliJ IDEA
  * founded at 2020/11/26 19:36:37
  * description:
- * 给定一个大小为 n 的整数数组，找出其中所有出现超过 ⌊ n/3 ⌋ 次的元素。
+ * 给定一个大小为 n 的整数数组，找出其中所有出现超过 ⌊ n / 3 ⌋ 次的元素。
  *
  * 进阶：尝试设计时间复杂度为 O(n)、空间复杂度为 O(1)的算法解决此问题。
  *
